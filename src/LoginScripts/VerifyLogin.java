1e016787121a521647bc431d3824a6030e96f975
sanjay <sanjaynegi1992@gmail.com>
2024-06-15 23:45:44 +0530

    Login Script

---
+++ src/LoginScripts/VerifyLogin.java
@@ -0,0 +1,38 @@
+package LoginScripts;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import POM.login;
+
+public class VerifyLogin {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Launch Browser
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demo.nopcommerce.com/");
+		
+		//Clicking on the login
+		driver.findElement(By.xpath("//a[text()='Log in']")).click();
+		
+		login Login = new login(driver);
+		Login.loginAction("rajeevsharma@gmail.com", "Abc@123");
+		
+		if(driver.getPageSource().contains("My account"))
+		{
+			System.out.println("Successful Login");
+		}
+		else
+		{
+			System.out.println("Wrong Crdentials");
+		}
+		
+		Thread.sleep(2000);
+		driver.quit();
+		}
+	}
