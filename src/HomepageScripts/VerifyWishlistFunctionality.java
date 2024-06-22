2e364a9dda9d9f0ca7445de0c7ed5e9dc1a46d53
sanjay <sanjaynegi1992@gmail.com>
2024-06-22 23:47:38 +0530

    Added verify wishlist script

--- src/HomepageScripts/VerifyWishlistFunctionality.java
+++ src/HomepageScripts/VerifyWishlistFunctionality.java
@@ -8,9 +8,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import com.google.common.io.Files;
-
 import POM.Homepage;
 import POM.login;
 
